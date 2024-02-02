caf830f79c9281edac542b1c93ae1e010e339ecf
bertieamorosev123 <142484348+bertieamorosev123@users.noreply.github.com>
2024-02-02 11:19:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '56196a47-2bc9-4f14-8e55-229651f61f2e';
 
-const proxyIPs = ['1.1.1.1'];
+const proxyIPs = ['104.16.148.244'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
